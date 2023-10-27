8698467b698374942e9f36d798935a972ba9a3c8
Artiommm Oaoao <ijezz@List.ru>
2023-10-27 21:03:39 +0300

    del

--- lab 2/lab_2_1_3 (incorrect).py
+++
@@ -1,6 +0,0 @@
-import random 
-with open("ip.log", "w") as ipLogs:
-    for i in range(0, 10000):
-        random.seed(i)
-        print(":".join("{0:04x}".format(ipv6) for ipv6 in [random.randint(0, 29999) for hexNumbers in range(8)]), file=ipLogs)
-ipLogs.close()
