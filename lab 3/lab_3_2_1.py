c97318c8fc420b9029792e38b82f61b5e7d80329
Artiommm Oaoao <ijezz@List.ru>
2023-11-07 02:59:15 +0300

    fix

--- lab 3/lab_3_2_1.py
+++ lab 3/lab_3_2_1.py
@@ -2,7 +2,7 @@ def reducer(arg: tuple) -> tuple:
     dividers = list(int(i) for i in range(1, arg[0]+1))
     filtred = list(filter(lambda div: arg[0] % div == 0 and arg[1] % div == 0, dividers))    
     m, n = map(lambda item: item / sorted(filtred, reverse=True)[0], arg)
-    ZeroDivisionError = m/n 
+    ZeroDivisionErr = m/n 
     return (m,n)
 
 while True:
